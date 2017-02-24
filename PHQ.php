c6e8e4344775927602222b7e9325174bb6cc080a
MiketheChap <mikethechap@gmail.com>
2017-02-24 12:08:28 -0600

    Update PHQ.php

--- PHQ.php
+++ PHQ.php
@@ -11,10 +11,29 @@ h1 {
     text-align: center;
 }
 
+h2 {
+    color: black;
+    text-align: center;
+	font-family: verdana;
+    font-size: 20px;
+}
+
 p {
     font-family: verdana;
     font-size: 20px;
 }
+
+h3 {
+    font-family: verdana;
+    font-size: 20px;
+}
+
+h4 {
+    font-family: verdana;
+    font-size: 14px;
+}
+
+
 .error {color: #FF0000;}
 </style>
 </head>
@@ -24,6 +43,10 @@ p {
 // define variables and set to empty values
 $hopeErr = $suicideErr = $commentErr = $websiteErr = "";
 $hope = $suicide = $comment = $website = "";
+$hopeNo = "<b>Question 1:</b> - You said you <i>are not feeling </i>hopeless or worthless. We're glad to hear that. But, in our study using the PHQ-9 as the gold standard, 20% of patients who said they weren't feeling hopeless or worthless, actually DID meet diagnostic criteria for a provisional diagnosis of Minor depression, Dysthymia, or Major Depression, mild.";
+$hopeYes = "<b>Question 1:</b> - You said you <i>are feeling</i> hopeless or worthless. The study data indicates that a positive response to this question generally is correct. However, compared to the PHQ-9, if you answered that you were NOT feeling hopeless or worthless here, about 20% of the time, this question would give wrong results.";
+$suicideNo = "<b>Question 2:</b> - You said you <i>are not feeling </i> thoughts of suicide or self-harm. Thankfully, you don't appear to be having any active suicidal ideation. Yeah! In the research study, there wasn't enough data from this question to make it statistically significant.";
+$suicideYes = "<b>Question 2:</b> - You said you <i>are having</i> thoughts of suicide or self-harm. Your response should trigger a call for follow-up and safety precautions, pending further assessment. One potential problem with this question, however, is that there is no look-back time frame. Moreover, there is no inquiry about previous suicidal behavior. Literature suggests that suicidal ideation + previous suicidal behavior comprise significant risk for suicide.";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if (empty($_POST["hope"])) {
@@ -88,14 +111,17 @@ function test_input($data) {
 </form>
 
 <?php
-echo "<h2>Here's your responses:</h2>";
-echo "<h3>Regarding you're feeling hopeless or worthless, you said: " . $hope ."</h3>";
-echo "<br>";
-echo $suicide;
-echo "<br>";
-echo $website;
-echo "<br>";
-echo $comment;
+echo "<h3>Here's your responses:</h3>";
+    if ($hope == "no")
+  echo "<h4>" . $hopeNo . "</h4>";
+    if ($hope == "yes")
+  echo "<h4>" . $hopeYes . "</h4>";
+    if ($suicide == "no")
+  echo "<h4>" . $suicideNo . "</h4>";
+    if ($suicide == "yes")
+  echo "<h4>" . $suicideYes . "</h4>";
+echo "<h4>You said your website is: " . $website ."</h4>";
+echo "<h4>And, here's your comment: " . $comment ."</h4>";
 echo "<br>";
 ?>
 
