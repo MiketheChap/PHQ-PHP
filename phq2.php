61a3cb079a3ac6947741d246333d7f238a74e677
MiketheChap <mikethechap@gmail.com>
2017-03-03 22:07:28 -0600

    Update phq2.php

--- phq2.php
+++ phq2.php
@@ -47,6 +47,10 @@ h3 {
 h4 {
     font-family: verdana;
 }
+
+warning {
+	color: red;
+}
 .error {color: #FF0000;}
 </style>
 </head>
@@ -54,10 +58,8 @@ h4 {
 
 <?php
 // define variables and set to empty values
-// you need to add the error variables for all 9 PHQ-9 questions below 
-$hopeErr = $suicideErr = $p1Err = $p2Err = $p3Err = "";
-// you need to add the regular variables for all 9 PHQ-9 questions below
-$hope = $suicide = $p1 = $p2 = $p3;
+$hopeErr = $suicideErr = $p1Err = $p2Err = $p3Err = $p4 = $p5 = $p6 = $p7 = $p8 = $p9 = "";
+$hope = $suicide = $p1 = $p2 = $p3 = $p4 = $p5 = $p6 = $p7 = $p8 = $p9 = "";
 $hopeNo = "<b>Question 1:</b> You said you <i>are not feeling </i>hopeless or worthless. We're glad to hear that. But, in our study comparing the CS to the PHQ-9, 20% of patients saying they were not feeling hopeless or worthless on the CS, actually met the diagnostic criteria for a provisional diagnosis of Minor depression, Dysthymia, or Major Depression, mild.";
 $hopeYes = "<b>Question 1:</b> You said you <i>are feeling</i> hopeless or worthless. The study data indicates that a positive response to this question generally is correct. However, compared to the PHQ-9, if you answered that you were NOT feeling hopeless or worthless here, about 20% of the time, this question would give wrong results.";
 $suicideNo = "<b>Question 2:</b> You said you <i>are not feeling </i> thoughts of suicide or self-harm. Thankfully, you don't appear to be having any active suicidal ideation. Yeah! In the research study, there wasn't enough data from this question to make it statistically significant.";
@@ -66,12 +68,10 @@ $howOften0 = "Not at all";
 $howOften1 = "Several days";
 $howOften2 = "More than half the days";
 $howOften3 = "Nearly every days"; 
-// you need to add the individual questions on the PHQ to get the total
-$phq_total = $p1 + $p2 + $p3;
 
-// for each question, you need to tell it what to do as far as the input goes
+$phq_total = $p1 + $p2 + $p3 + $p4 + $p5 + $p6 + $p7 + $p8 + $p9;
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // hope
   if (empty($_POST["hope"])) {
     $hopeErr = "The question about hope is required";
   } else {
@@ -83,29 +83,62 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $suicideErr = "The answer about suicide is required";
   } else {
     $suicide = test_input($_POST["suicide"]);
-    // check if name only contains letters and whitespace
   }
     
   if (empty($_POST["p1"])) {
     $p1Err = "The first question on the PHQ-9 is required.";
   } else {
     $p1 = test_input($_POST["p1"]);
-	// check if name only contains letters and whitespace
     }
 
   if (empty($_POST["p2"])) {
     $p2Err = "The second question on the PHQ-9 is required.";
   } else {
     $p2 = test_input($_POST["p2"]);
-	// check if name only contains letters and whitespace
   }
   
   if (empty($_POST["p3"])) {
     $p3Err = "The third question on the PHQ-9 is required.";
   } else {
     $p3 = test_input($_POST["p3"]);
-	// check if name only contains letters and whitespace
   }
+  
+  if (empty($_POST["p4"])) {
+    $p3Err = "The fourth question on the PHQ-9 is required.";
+  } else {
+    $p4 = test_input($_POST["p4"]);
+  }
+  
+  if (empty($_POST["p5"])) {
+    $p3Err = "The fifth question on the PHQ-9 is required.";
+  } else {
+    $p5 = test_input($_POST["p5"]);
+  }
+  
+  if (empty($_POST["p6"])) {
+    $p3Err = "The sixth question on the PHQ-9 is required.";
+  } else {
+    $p6 = test_input($_POST["p6"]);
+  }
+  
+  if (empty($_POST["p7"])) {
+    $p3Err = "The seventh question on the PHQ-9 is required.";
+  } else {
+    $p7 = test_input($_POST["p7"]);
+  }
+  
+  if (empty($_POST["p8"])) {
+    $p3Err = "The eighth question on the PHQ-9 is required.";
+  } else {
+    $p8 = test_input($_POST["p8"]);
+  }
+  
+  if (empty($_POST["p9"])) {
+    $p3Err = "The ninth question on the PHQ-9 is required.";
+  } else {
+    $p9 = test_input($_POST["p9"]);
+  }
+  
 }
 function test_input($data) {
   $data = trim($data);
@@ -116,7 +149,7 @@ function test_input($data) {
 ?>
 
 <h1>Current Screen (CS) Compared to the Patient Health Questionnaire-9 (PHQ-9) Study</h1>
-<h3>You can take both screens right here!</h3>
+<h3>You can take both screens right here! No personal information of any type is desired or saved. However, do not enter anything relevant to you own personal experience. After you hit the Submit Button, your scores and alerts will be shown underneath the Submit Button.</h3>
 <hr>
 <h3>The <b>Current Screen</b> asks two questions:</h3>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
@@ -132,6 +165,7 @@ function test_input($data) {
   <br><br>
   <hr>
   <h3>The Patient Health Questionnaire-9 (PHQ-9)asks nine questions:</h3>
+  
   <h4>In the <b>past two weeks</b> how often have you been bothered by any of the following problems:</h4>
   <h5>1. Little interest or pleasure in doing things</h5>
   <input type="radio" name="p1" <?php if (isset($p1) && $p1=="0") echo "checked";?> value="0"> Not At All<br>
@@ -139,6 +173,7 @@ function test_input($data) {
   <input type="radio" name="p1" <?php if (isset($p1) && $p1=="2") echo "checked";?> value="2"> More than half the days<br>
   <input type="radio" name="p1" <?php if (isset($p1) && $p1=="3") echo "checked";?> value="3"> Nearly every day
   <span class="error">* <?php echo $p1Err;?></span>
+  
   <h5>2. Feeling down, depressed or hopeless</h5>
   <input type="radio" name="p2" <?php if (isset($p2) && $p2=="0") echo "checked";?> value="0"> Not At All<br>
   <input type="radio" name="p2" <?php if (isset($p2) && $p2=="1") echo "checked";?> value="1"> Several Days<br>
@@ -153,13 +188,60 @@ function test_input($data) {
   <input type="radio" name="p3" <?php if (isset($p3) && $p3=="3") echo "checked";?> value="3"> Nearly every day
   <span class="error">* <?php echo $p3Err;?></span>
   
+  <h5>4. Feeling tired or having little energy?</h5>
+  <input type="radio" name="p4" <?php if (isset($p4) && $p4=="0") echo "checked";?> value="0"> Not At All<br>
+  <input type="radio" name="p4" <?php if (isset($p4) && $p4=="1") echo "checked";?> value="1"> Several Days<br>
+  <input type="radio" name="p4" <?php if (isset($p4) && $p4=="2") echo "checked";?> value="2"> More than half the days<br>
+  <input type="radio" name="p4" <?php if (isset($p4) && $p4=="3") echo "checked";?> value="3"> Nearly every day
+  <span class="error">* <?php echo $p4Err;?></span>
+  
+  <h5>5. Poor appetite or overeating?</h5>
+  <input type="radio" name="p5" <?php if (isset($p5) && $p5=="0") echo "checked";?> value="0"> Not At All<br>
+  <input type="radio" name="p5" <?php if (isset($p5) && $p5=="1") echo "checked";?> value="1"> Several Days<br>
+  <input type="radio" name="p5" <?php if (isset($p5) && $p5=="2") echo "checked";?> value="2"> More than half the days<br>
+  <input type="radio" name="p5" <?php if (isset($p5) && $p5=="3") echo "checked";?> value="3"> Nearly every day
+  <span class="error">* <?php echo $p5Err;?></span>
+  
+  <h5>6. Feeling bad about yourself — or that you are a failure or
+have let yourself or your family down?</h5>
+  <input type="radio" name="p6" <?php if (isset($p6) && $p6=="0") echo "checked";?> value="0"> Not At All<br>
+  <input type="radio" name="p6" <?php if (isset($p6) && $p6=="1") echo "checked";?> value="1"> Several Days<br>
+  <input type="radio" name="p6" <?php if (isset($p6) && $p6=="2") echo "checked";?> value="2"> More than half the days<br>
+  <input type="radio" name="p6" <?php if (isset($p6) && $p6=="3") echo "checked";?> value="3"> Nearly every day
+  <span class="error">* <?php echo $p6Err;?></span>
+  
+  <h5>7. Trouble concentrating on things, such as reading the
+newspaper or watching television?</h5>
+  <input type="radio" name="p7" <?php if (isset($p7) && $p7=="0") echo "checked";?> value="0"> Not At All<br>
+  <input type="radio" name="p7" <?php if (isset($p7) && $p7=="1") echo "checked";?> value="1"> Several Days<br>
+  <input type="radio" name="p7" <?php if (isset($p7) && $p7=="2") echo "checked";?> value="2"> More than half the days<br>
+  <input type="radio" name="p7" <?php if (isset($p7) && $p7=="3") echo "checked";?> value="3"> Nearly every day
+  <span class="error">* <?php echo $p6Err;?></span>
+  
+  <h5>8. Moving or speaking so slowly that other people could have
+noticed? Or the opposite — being so fidgety or restless
+that you have been moving around a lot more than usual?</h5>
+  <input type="radio" name="p8" <?php if (isset($p8) && $p8=="0") echo "checked";?> value="0"> Not At All<br>
+  <input type="radio" name="p8" <?php if (isset($p8) && $p8=="1") echo "checked";?> value="1"> Several Days<br>
+  <input type="radio" name="p8" <?php if (isset($p8) && $p8=="2") echo "checked";?> value="2"> More than half the days<br>
+  <input type="radio" name="p8" <?php if (isset($p8) && $p8=="3") echo "checked";?> value="3"> Nearly every day
+  <span class="error">* <?php echo $p6Err;?></span>
+  
+  <h5>9. Thoughts that you would be better off dead or of hurting
+yourself in some way?</h5>
+  <input type="radio" name="p9" <?php if (isset($p9) && $p9=="0") echo "checked";?> value="0"> Not At All<br>
+  <input type="radio" name="p9" <?php if (isset($p9) && $p9=="1") echo "checked";?> value="1"> Several Days<br>
+  <input type="radio" name="p9" <?php if (isset($p9) && $p9=="2") echo "checked";?> value="2"> More than half the days<br>
+  <input type="radio" name="p9" <?php if (isset($p9) && $p9=="3") echo "checked";?> value="3"> Nearly every day
+  <span class="error">* <?php echo $p6Err;?></span>
+  
   
   <br><br>
   <input type="submit" name="submit" value="Submit">  
 </form>
 
 <?php
-$phq_total = $p1 + $p2 +$p3;
+$phq_total = $p1 + $p2 +$p3 + $p4 + $p5 + $p6 + $p7 + $p8 + $p9;
 
 echo "<h3>When you've completed the above forms, your answers will show below:</h3>";
 echo "<hr>";
@@ -181,13 +263,26 @@ echo "<li>1 = Several days</li>";
 echo "<li>2 = More than half the days</li>";
 echo "<li>2 = Nearly every day</li>";
 echo "</ul>";
-// add results for each question below and conclude with the total
-echo "<h4>Your score on little interest or pleasure was " . $p1 . "</h4>";
-echo "<h4>Your score on feeling down, depressed or hopeless was " . $p2 . "</h4>";
-echo "<h4>Your score on falling asleep, staying asleep or sleeping too much was " . $p3 . "</h4>";
+echo "<h4>Your score on (1) little interest or pleasure was " . $p1 . "</h4>";
+echo "<h4>Your score on (2) feeling down, depressed or hopeless was " . $p2 . "</h4>";
+echo "<h4>Your score on (3) falling asleep, staying asleep or sleeping too much was " . $p3 . "</h4>";
+echo "<h4>Your score on (4) feeling tired or having little energy was " . $p4 . "</h4>";
+echo "<h4>Your score on (5) poor appetite or overeating was " . $p5 . "</h4>";
+echo "<h4>Your score on (6) feeling bad about yourself — or that you are a failure or
+have let yourself or your family down - was " . $p6 . "</h4>";
+echo "<h4>Your score on (7) trouble concentrating on things, such as reading the
+newspaper or watching television  " . $p7 . "</h4>";
+echo "<h4>Your score on(8) moving or speaking so slowly that other people could have
+noticed? Or the opposite — being so fidgety or restless
+that you have been moving around a lot more than usual was  " . $p8 . "</h4>";
+echo "<h4>Your score on (9) thoughts that you would be better off dead or of hurting
+yourself in some way was  " . $p9 . "</h4>";
+if ($p9  > 0)
+  echo "<h4><warning>Your score indicates that you may be at risk for suicide. Someone should be in close observation until you further assessed and cleared.</warning></h4>";
 echo "<h4>Your overall score on the PHQ-9 was: " . $phq_total ."</h4>";
+if ($phq_total  > 20)
+  echo "<h4><warning>Your overall PHQ-9 score indicates that you may be at high risk for suicide. Someone should be in close observation until you are cleared.</warning></h4>";
 echo "<br>";
 ?>
-
 </body>
 </html>
